12f4e0c448ef366f32794a2199d41902681f0c99
鸟 月月 <1689759238@qq.com>
2025-11-09 21:31:14 +0800

    字符串拓展

---
+++ Assets/Scripts/Yueyn/Utils/StringExpense.cs
@@ -0,0 +1,8 @@
+namespace Yueyn.Utils
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string str)=> string.IsNullOrEmpty(str);
+        public static bool IsNullOrWhiteSpace(this string str) => string.IsNullOrWhiteSpace(str);
+    }
+}
