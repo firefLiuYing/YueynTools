61c66f1100fd7f28b7c25fda3b2e94d7fc6999b7
鸟 月月 <1689759238@qq.com>
2025-11-10 14:16:08 +0800

    Variable支持事件订阅与通知

--- Assets/Scripts/Yueyn/Base/Variable/GenericVariable.cs
+++ Assets/Scripts/Yueyn/Base/Variable/GenericVariable.cs
@@ -1,14 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 namespace Yueyn.Base.Variable
 {
-    public abstract class Variable<T> : Variable
+    public class Variable<T> : Variable
     {
         public override Type Type=>typeof(T);
 
-        public T Value { get; set; } = default(T);
+        public delegate void Listener(T value);
+
+        private readonly List<Listener> _listeners = new();
+        public void Observe(Listener listener)=>_listeners.Add(listener);
+        public void Unobserve(Listener listener)=>_listeners.Remove(listener);
+        public T Value { get; private set; } = default(T);
         public override object GetValue()=>Value;
         public override void SetValue(object value)=>Value = (T)value;
-        public override void Clear()=>Value=default(T);
+
+        public void PostValue(T value)
+        {
+            foreach (var listener in _listeners)
+            {
+                listener.Invoke(value);
+            }
+        }
+
+        public void PostValue() => PostValue(Value);
+        public override void Clear()
+        {
+            Value=default(T);
+            _listeners.Clear();
+        }
     }
 }
